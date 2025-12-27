1e45703853ced1890887aa33483f743ceee3dfba
Adrian Toman <adrian@adriantoman.com>
2025-12-27 14:15:59 +1100

    feat: Add unit test project

---
+++ tests/FunctionApp.UnitTests/TrafficIngestionServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using FunctionApp.Services;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+
+namespace FunctionApp.UnitTests;
+
+public class TrafficIngestionServiceTests
+{
+    [Fact]
+    public async Task RunAsync_CompletesSuccessfully()
+    {
+        // Arrange
+        var loggerMock = Substitute.For<ILogger<TrafficIngestionService>>();
+        var service = new TrafficIngestionService(loggerMock);
+
+        // Act & Assert
+        await service.RunAsync();
+    }
+}
